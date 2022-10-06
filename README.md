76c5dd7b4548c8ebf4c063294db0bd2299214143
isabelle galvão <isabellegalvao123@gmail.com>
2022-10-06 20:10:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Back-end
+
+## Entidades
+
+## Game
+
+### AD
+id
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+huorEnd
+useVoiceChannel
+createAdt
+
+
+
+## Casos de us
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express, { request, response } from 'express'
+
+const app = express()
+
+// localhost:3333/ads
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('ads/',(request, response)=> {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (Request, response) => {
+  //const gameID = request.params.id;
+
+  return response.json([
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' }
+  ])
+})
+
+app.get('/ads/:id/discord', (Request, response) => {
+  //const adId = request.params.id;
+
+  return response.json([])
+})
+
+app.listen(3333)
